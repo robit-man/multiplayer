835694e57d3334bb559ca4b4bcdf5ad010c0e656
robit-man <coleman.k.myers@gmail.com>
2024-12-21 00:14:09 -0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  assetsInclude: ['**/*.glb'],
+  assetsInclude: ['**/*.glb'], // Include .glb as assets
+  publicDir: 'public', // Ensure Vite processes the /public folder
+  build: {
+    outDir: 'dist', // Specify the build output directory
+    assetsDir: '', // Keep assets like .glb at the root of /dist
+  },
 });
