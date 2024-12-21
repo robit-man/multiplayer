3c213997000a1c8b5ebe9c58a59603696389957c
robit <36677806+robit-man@users.noreply.github.com>
2024-12-20 20:21:57 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   build: {
     outDir: 'dist', // Directory for build output
     emptyOutDir: true, // Clean the build directory before output
+    rollupOptions: {
+      external: ['fsevents'], // Exclude 'fsevents' to avoid build issues
+    },
   },
   server: {
     port: 3000, // Local development server port
